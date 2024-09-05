e2263f66cfacb63f5968cfabd036bccc088ff3c9
Anushree C <147231895+anushree200@users.noreply.github.com>
2024-09-05 16:43:02 +0530

    Update instr_IS.h

--- instr_IS.h
+++ instr_IS.h
@@ -2,5 +2,6 @@
 #define INSTRUCTION_IS_H
 
 void process_instructionIS(char *line, unsigned int funct3, unsigned int opcode, int n);
+void SRAI_IS(char *line, unsigned int funct3, unsigned int opcode, int n);
 
 #endif
