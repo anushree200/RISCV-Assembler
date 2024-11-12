cc10100356d80723e6ef62ff48bda911e3f83059
Anushree C <147231895+anushree200@users.noreply.github.com>
2024-11-12 23:49:13 +0530

    Delete instr_IS.h

--- instr_IS.h
+++
@@ -1,7 +0,0 @@
-#ifndef INSTRUCTION_IS_H
-#define INSTRUCTION_IS_H
-
-void process_instructionIS(char *line, unsigned int funct3, unsigned int opcode, int n);
-void SRAI_IS(char *line, unsigned int funct3, unsigned int opcode, int n);
-
-#endif
